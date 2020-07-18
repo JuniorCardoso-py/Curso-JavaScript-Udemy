7260717ea28d42b0b3e38f5643e4a4793cf90bf7
Junior <juniorcardoso.sc@outlook.com>
2020-07-18 17:05:35 -0300

    Criado Pastas para aulas

---
+++ Aula27/index.js
@@ -0,0 +1,18 @@
+const numero = 0;
+
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O Numero esta entre 0 e 5')
+}
+else if (numero >= 6 && numero <= 8) {
+    console.log('O Numero esta entre 6 e 8')
+}
+else if (numero >= 9 && numero <= 11) {
+    console.log('O Numero esta entre 9 e 11')
+}
+else {
+    console.log('O Numero não esta entre 0 e 11')
+}
+
+
+console.log('Aqui sempre sera exibido apos os ifs')
